a6e5f684554d8b51a3b35b562e1e03d7069a62f5
ivansabater <ivansabater@gmail.com>
2022-03-01 15:43:31 +0100

    crear Seeder User y Model

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        DB::table('users')->delete();
+        // Añadimos una entrada a esta tabla
+        DB::table('users')->insert([
+        'name' => 'Username',
+        'type' => 'Lector',
+        'email' => 'name@domain.com ',
+        'password' => 'strongpassword',
+        'telephone' => '966999999'
+    ]);
+    }
+}
