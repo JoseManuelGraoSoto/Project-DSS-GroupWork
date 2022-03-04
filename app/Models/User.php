04a844f1d863b85d9b0bf4cfaed92a747eee2a68
Carlos Zambrana <71760631+carlosky66@users.noreply.github.com>
2022-03-04 23:03:01 +0100

    Modificando el nombre de un metodo

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,7 +42,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function articles() {
+    public function user_articles() {
         return $this->belongsToMany('App\Models\Article');
     }
 }
