d9ba3e9aeae577ea46045f21ddff30279c9ff837
Jose Carlos Zambrana Navajas <jczn1@alu.ua.es>
2022-03-21 14:32:58 +0100

    Clase controlador de user creada y alguna vista de ejemplo

---
+++ resources/views/userProfile.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <body class="antialiased">
+        <div>
+            Hola, {{$user->name}}
+            <br>
+            Email -> {{$user->email}}
+            <br>
+            Password -> {{$user->password}} 
+        </div>
+    </body>
+</html>
---
+++ resources/views/welcome2.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <body class="antialiased">
+    <a href="{{ route('user.show', ['id' => $id]) }}">
+        Ver perfil de usuario
+    </a>
+    </body>
+</html>
