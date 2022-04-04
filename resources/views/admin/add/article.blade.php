e39a5385f71e2e540ffa3ca53b2ec4de548c8caf
RosiYo <arp129@alu.ua.es>
2022-04-04 13:30:07 +0200

    Cambios temporales

--- resources/views/admin/add/article.blade.php
+++ resources/views/admin/add/article.blade.php
@@ -6,8 +6,8 @@
 @endsection
 
 @section('other-inputs')
-<div class="inputs container flex-vertical flex-center flex-aligned-center">
-    <select name="category" id="category-input">
+<div class="inputs1 container flex-vertical flex-center flex-aligned-center">
+    <select name="category" id="category-input" style="margin-bottom: 40px;">
         <option selected value="default" id="default-category">Seleccione una categoría</option>
         <option value="Categoria1">Categoria1</option>
         <option value="Categoria2">Categoria2</option>
@@ -18,12 +18,21 @@
         <input type="file" id="selec-txt"/>
         <label for="selec-img">Contenido del artículo</label>
     </div>
+</div>
 
-    <input type="number" name="valoration" id="valoration" placeholder="Valoración general del artículo">
-
-    <div class="checkbox-con">
-        <span style="color: black;">¿Aceptado?</span>
+<div class="inputs2 container flex-vertical flex-center flex-aligned-center">
+    <div class="container flex-vertical flex-center flex-aligned-center">
+        <span style="color: var(--primary-color);">Valoración general del artículo</span>
+        <div class="number-input">
+            <button onclick="this.parentNode.querySelector('input[type=number]').stepDown()" ></button>
+            <input class="quantity" min="0" max="10" step=".5" name="quantity" value="1" type="number">
+            <button onclick="this.parentNode.querySelector('input[type=number]').stepUp()" class="plus"></button>
+        </div>
+    </div>
+    
+    <div class="checkbox-con container flex-vertical flex-center flex-aligned-center">
         <input id="checkbox" type="checkbox">
+        <span style="color: var(--primary-color);">¿Aceptado?</span>
     </div>
 </div>
 @endsection
