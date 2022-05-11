89d418903ab8a50c12dea7e06f4fc8c5a4d063a6
RosiYo <arp129@alu.ua.es>
2022-05-11 17:02:22 +0200

    Primera versión perfil de usuario

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use App\Http\Controllers\ValorationController;
 */
 
 // Prueba
-Route::get('/', function () { return view('common.article'); });
+Route::get('/', function () { return view('common.profile'); });
 
 Route::get('/loggin', function () {
     return view('access.login');
