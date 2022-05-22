4f8e2a128c01b3bac319ec1fd6886aa9d0e17a1a
ivansabater <ivansabater@gmail.com>
2022-05-22 22:54:08 +0200

    arreglar vista

--- resources/views/admin/add/createUser.blade.php
+++ resources/views/admin/add/createUser.blade.php
@@ -9,7 +9,7 @@
 
 @section('select-img')
 <div class="user-image-selection flex-container flex-vertical flex-aligned-center flex-center">
-    <img class="user-image" src="getVideo()" id="foto_add" alt="Imagen del usuario">
+    <img class="user-image" src="{{URL::asset('img/default.png');}}" id="foto_add" alt="Imagen del usuario">
     <div class="upload-img">
         <input type="file" id="selec-img" name="selec-img" />
         <label for="selec-img">Selecciona imagen</label>
