c32f8851cbbed6afffbaf39a9fe169263df5c777
Carlos Zambrana <71760631+carlosky66@users.noreply.github.com>
2022-05-29 11:44:14 +0200

    Arreglado borrado de categorias

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -22,7 +22,7 @@
                 @foreach($categorys as $category)
                 <li class="flex-container flex-aligned-center checkbox-container">
                     <div class="checkbox path">
-                        <input id="{{$category->category}}-checkbox" type="checkbox" name="category" value="{{$category->id}}">
+                        <input id="{{$category->category}}-checkbox" class="category" type="checkbox" name="{{$category->category}}-checkbox" value="{{$category->id}}">
                         <svg viewBox="0 0 21 21">
                             <path d="M5,10.75 L8.5,14.25 L19.4,2.3 C18.8333333,1.43333333 18.0333333,1 17,1 L4,1 C2.35,1 1,2.35 1,4 L1,17 C1,18.65 2.35,20 4,20 L17,20 C18.65,20 20,18.65 20,17 L20,7.99769186"></path>
                         </svg>
@@ -83,8 +83,8 @@
 </script>
 
 <script>
-    $('input[name=category]').change(function() {
-        let checked = $('input[name=category]:checked');
+    $('.category').change(function() {
+        let checked = $('.category:checked');
 
         if (checked.length != 0) {
             $('#delete-btn').fadeIn();
