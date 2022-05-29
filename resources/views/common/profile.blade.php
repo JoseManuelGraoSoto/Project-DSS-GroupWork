f319e718c45dd6fa80c1ba5bde35d67dd30180ce
Carlos Zambrana <71760631+carlosky66@users.noreply.github.com>
2022-05-29 17:28:17 +0200

    .

--- resources/views/common/profile.blade.php
+++ resources/views/common/profile.blade.php
@@ -159,6 +159,7 @@
                             <div class="d-flex justify-content-between align-items-center mb-3 ms-3">
                                 <small>Fecha de caducidad de suscripción</small>
                                 <small>{{Auth::user()->endSubscriptionDate}}</small>
+
                             </div>
 
                         </div>
